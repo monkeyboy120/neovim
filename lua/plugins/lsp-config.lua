4c323bcfa47081c8c74763310d0c7ac60762ceef
Joshua La <j0shua001@outlook.com>
2024-04-07 00:15:48 -0400

    working on debugger

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -37,6 +37,9 @@ return {
 			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.r_language_server.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
