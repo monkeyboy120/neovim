7e75d4cc6255d2c4afde9ab4bceb1aa8e5099946
Joshua La <j0shua001@outlook.com>
2024-09-09 23:29:33 -0400

    debugger 100% working c++

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -4,29 +4,31 @@ return {
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
 		"theHamsta/nvim-dap-virtual-text",
-		"jay-baba/mason-nvim-dap.nvim"
+		"jay-baba/mason-nvim-dap.nvim",
 	},
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
 
 		require("dapui").setup()
-		require("nvim-dap-virtual-text").setup()
-		require("mason-nvim-dap").setup({
-			ensure_installed = { " codelldb " },
+		require("nvim-dap-virtual-text").setup({
+			enabled = true,
+			enable_commands = true,
+			highlight_changed_variables = true,
+			highlight_new_as_changed = true,
+			show_stop_reason = true,
+			commented = false,
+			virt_text_pos = "eol",
 		})
-
-		dap.adapters.gdp = {
-			type = "executable",
-			command = "gdb",
-			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
-		}
+		require("mason-nvim-dap").setup({
+			ensure_installed = { "codelldb" },
+		})
 
 		dap.adapters.lldb = {
 			type = "server",
 			port = "${port}",
 			executable = {
-				command = "~/.local/share/nvim/mason/bin/codelldb",
+				command = vim.fn.stdpath("data") .. "/mason/bin/codelldb", -- Correct path
 				args = { "--port", "${port}" },
 			},
 		}
@@ -40,51 +42,32 @@ return {
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
-				stopAtBeginningOfMainSubprogram = false,
-			},
-			{
-				name = "Select and attach to process",
-				type = "lldb",
-				request = "attach",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				pid = function()
-					local name = vim.fn.input("Executable name (filter): ")
-					return require("dap.utils").pick_process({ filter = name })
-				end,
-				cwd = "${workspaceFolder}",
-			},
-			{
-				name = "Attach to gdbserver :1234",
-				type = "lldb",
-				request = "attach",
-				target = "localhost:1234",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
+				args = {},
 			},
 		}
 
 		dap.configurations.rust = dap.configurations.cpp
 
-		dap.listeners.before.attach.dapui_config = function()
+		-- DAP UI listeners to open/close automatically
+		dap.listeners.after.event_initialized["dapui_config"] = function()
 			dapui.open()
 		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
+		dap.listeners.before.event_terminated["dapui_config"] = function()
 			dapui.close()
 		end
-		dap.listeners.before.event_exited.dapui_config = function()
+		dap.listeners.before.event_exited["dapui_config"] = function()
 			dapui.close()
 		end
 
+		-- Keybindings for debugging
 		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dC", ":DapToggleConditionalBreakopint<CR")
+		vim.keymap.set("n", "<Leader>dk", ":DapToggleAllBreakpoints<CR>")
 		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
 		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
 		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+		vim.keymap.set("n", "<Leader>di", ":DapStepInto<CR>")
+		vim.keymap.set("n", "<Leader>ds", ":DapStepOut<CR>")
+		vim.keymap.set("n", "<Leader>dr", ":DapRestart<CR>")
 	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -45,10 +45,13 @@ return {
 			})
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
-			})
+		})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>S", ":ClangdSwitchSourceHeader<CR>", {})
 		end,
 	},
 }
