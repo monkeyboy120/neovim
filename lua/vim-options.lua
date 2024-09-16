e108f70c7a0022367788f2d35a70a97811e7daa8
Joshua La <j0shua001@outlook.com>
2024-09-16 14:33:47 -0400

    added tmux to dotfiles

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,9 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+-- Set tab width to 2 spaces for C++ files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "cpp", "hpp" },
+    command = "setlocal tabstop=2 shiftwidth=2 expandtab"
+})
