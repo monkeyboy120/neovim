516d4e908d94653e6d198a8341155b3f95400ab8
Joshua La <j0shua001@outlook.com>
2025-01-14 12:46:42 -0500

    makefile added to lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -46,6 +46,9 @@ return {
 			lspconfig.zls.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.checkmake.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
